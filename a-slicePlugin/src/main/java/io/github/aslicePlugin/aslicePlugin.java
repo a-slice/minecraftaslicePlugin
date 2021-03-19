1c6ac94c7beadfd0998bd627708df96e4b3133c9
a-slice <alicemellanby2@gmail.com>
2021-03-19 22:39:18 +1300

    add kit command

--- a-slicePlugin/src/main/java/io/github/aslicePlugin/aslicePlugin.java
+++ a-slicePlugin/src/main/java/io/github/aslicePlugin/aslicePlugin.java
@@ -1,14 +1,17 @@
 package io.github.aslicePlugin;
 
+import io.github.aslicePlugin.commands.kit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class aslicePlugin extends JavaPlugin {
     @Override
     public void onEnable() {
-        getLogger().info("onEnable is called!");
+        getLogger().info("aslicePlugin enabled successfully");
+        //Register our command "kit" (set an instance of your command class as executor)
+        this.getCommand("kit").setExecutor(new kit());
     }
     @Override
     public void onDisable() {
-        getLogger().info("onDisable is called!");
+        getLogger().info("aslicePlugin disabled :(");
     }
 }
---
+++ a-slicePlugin/src/main/java/io/github/aslicePlugin/commands/kit.java
@@ -0,0 +1,29 @@
+package io.github.aslicePlugin.commands;
+
+import org.bukkit.Material;
+import org.bukkit.command.*;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class kit implements CommandExecutor {
+
+    // This method is called, when somebody uses our command
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender != null) {
+            Player player = (Player) sender;
+            // Here we need to give items to our player
+            // Create a new ItemStack (type: diamond)
+            ItemStack diamond = new ItemStack(Material.DIAMOND, 1);
+
+            // Create a new ItemStack (type: brick)
+            ItemStack spawneggs = new ItemStack(Material.PIG_SPAWN_EGG, 2);
+
+            // Give the player our items (comma-seperated list of all ItemStack)
+            player.getInventory().addItem(spawneggs, diamond);
+        }
+
+        // If the player (or console) uses our command correct, we can return true
+        return true;
+    }
+}
